74ac32012f61d2f441eb964970ca802dd4175c96
Laura <lg3295@gmail.com>
2016-04-02 12:07:14 -1000

    creates dom elements for body, container, cards, and title

--- public/js/client.js
+++ public/js/client.js
@@ -15,51 +15,34 @@ function requestListener(){
   var dataOverload = JSON.parse(this.responseText);
   console.log('dataOverload',dataOverload);
 
-  var body = document.getElementById('body');
-  var city = document.createElement('header');
-  city.innerHTML = dataOverload.city.name;
-  body.appendChild(city);
+  var container = document.getElementById('container');
+  var city = document.getElementById('cityName');
+  city.innerHTML = dataOverload.city.name + " Forecast";
 
   for (var i = 0; i < dataOverload.list.length; i++){
     var cardObj = document.createElement('div');
-    cardObj.classList.add = "cardObj";
+    cardObj.className = "cardObj";
     cardObj.id = dataOverload.list[i].dt_txt;
     cardObj.innerHTML = dataOverload.list[i].dt_txt + i;
-    body.appendChild(cardObj);
+    container.appendChild(cardObj);
 
     var description = document.createElement('div');
-    description.classList.add = "desc";
+    description.className = "desc";
     description.id = dataOverload.list[i].dt_txt + i + "d";
     description.innerHTML = dataOverload.list[i].weather[0].description;
     cardObj.appendChild(description);
 
     var temp = document.createElement('div');
-    temp.classList.add = "temp";
+    temp.className = "temp";
     temp.id = dataOverload.list[i].dt_txt + i + "t";
     temp.innerHTML = Math.round(((dataOverload.list[i].main.temp - 273.15)*1.8) +32) + " degrees Fahrenheit";
     cardObj.appendChild(temp);
 
     var humidity = document.createElement('div');
-    humidity.classList.add = "hum";
+    humidity.className = "hum";
     humidity.id = dataOverload.list[i].dt_txt + i + "h";
-    humidity.innerHTML = "Humidity: " + dataOverload.list[i].main.humidity;
+    humidity.innerHTML = "Humidity: " + dataOverload.list[i].main.humidity + "%";
     cardObj.appendChild(humidity);
   }
 
-
-
-
 }; //End of Request Listener
-
-
-//date
-//description
-//temp
-//humidity
-
-
-// create a city headline > append to window
-// create a card object >append to window
-// create a description, temp, humidity div > append to card object
-
-// stretch, create an icon and append to card
